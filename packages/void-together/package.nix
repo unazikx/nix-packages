7cc053c4a2032f226f4e4522ed724113d10fcd44
unazikx <xfalwa@gmail.com>
2026-02-22 21:05:12 +0900

    void-together: init

---
+++ packages/void-together/package.nix
@@ -0,0 +1,37 @@
+{
+  buildNpmPackage,
+  fetchFromGitHub,
+  makeWrapper,
+  lib,
+}:
+
+buildNpmPackage (old: {
+  pname = baseNameOf ./.;
+  version = "1.0.1";
+
+  src = fetchFromGitHub {
+    owner = "VoidTogether";
+    repo = "VoidTogether-Server";
+    rev = "f00208957b08d82702c5f4b9b07ee56c87da46bb";
+    hash = "sha256-mjzV36f0sORxELKmdfxXSmlX9VCOykobQR37bCylluQ=";
+  };
+
+  npmDepsHash = "sha256-0OevmrVW2yk0nMqcFbjvx8xjyXLuuDxBYU2//nDHuI8=";
+  dontNpmBuild = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  npmInstallFlags = "ci";
+  installPhase = ''
+    mkdir -p $out/bin $out/share/void-together
+    cp -r * $out/share/void-together
+  '';
+
+  meta = {
+    description = "VOTV server for multiplayer";
+    homepage = "https://github.com/VoidTogether/VoidTogether-Server";
+    license = lib.licenses.agpl3Only; # it is unfree
+    platforms = [ "x86_64-linux" ];
+    maintainers = with lib.maintainers; [ azikx ];
+  };
+})
