6f036c53f47453dc40217a9da5866f2bd2e01d00
unazikx <xfalwa@gmail.com>
2026-04-08 16:48:20 +0900

    flake: im shizo

--- flake.nix
+++ flake.nix
@@ -1,26 +1,62 @@
 {
+  description = ''
+    .__   __.  __  ___   ___ 
+    |  \ |  | |  | \  \ /  / 
+    |   \|  | |  |  \  V  /  
+    |  . `  | |  |   >   <   
+    |  |\   | |  |  /  .  \  
+    |__| \__| |__| /__/ \__\ 
+                             
+    .______      ___       ______  __  ___      ___       _______  _______      _______.
+    |   _  \    /   \     /      ||  |/  /     /   \     /  _____||   ____|    /       |
+    |  |_)  |  /  ^  \   |  ,----'|  '  /     /  ^  \   |  |  __  |  |__      |   (----`
+    |   ___/  /  /_\  \  |  |     |    <     /  /_\  \  |  | |_ | |   __|      \   \    
+    |  |     /  _____  \ |  `----.|  .  \   /  _____  \ |  |__| | |  |____ .----)   |   
+    | _|    /__/     \__\ \______||__|\__\ /__/     \__\ \______| |_______||_______/    
+  '';
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    pkgs-by-name.url = "github:drupol/pkgs-by-name-for-flake-parts";
+    nixpkgs.follows = "nixpkgs-unstable";
+
+    nixpkgs-unstable = {
+      type = "github";
+      owner = "nixos";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+
+    pkgs-by-name = {
+      type = "github";
+      owner = "drupol";
+      repo = "pkgs-by-name-for-flake-parts";
+    };
 
     flake-parts = {
-      url = "github:hercules-ci/flake-parts";
+      type = "github";
+      owner = "hercules-ci";
+      repo = "flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
     nur = {
-      url = "github:nix-community/NUR";
+      type = "github";
+      owner = "nix-community";
+      repo = "NUR";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
 
     treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
+      type = "github";
+      owner = "numtide";
+      repo = "treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     emmanuelrosa-nix = {
-      url = "github:emmanuelrosa/erosanix";
+      type = "github";
+      owner = "emmanuelrosa";
+      repo = "erosanix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
