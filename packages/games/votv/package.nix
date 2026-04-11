fa7dd9e28f70629e3b5ca790f1819813e48355eb
unazikx <xfalwa@gmail.com>
2026-04-11 16:58:45 +0900

    votv: revert

--- packages/games/votv/package.nix
+++ packages/games/votv/package.nix
@@ -1,75 +1,36 @@
 {
-  mkWindowsApp,
+  stdenv,
   fetchurl,
   p7zip,
-  wine,
-  ouch,
   lib,
 }:
 
-let
-  gameDir = "$WINEPREFIX/drive_c/Program Files/VotV";
-in
-
-mkWindowsApp rec {
+stdenv.mkDerivation (_old: {
   pname = "voices-of-the-void";
   version = "a09j_0001";
 
   src = fetchurl {
-    url = "https://archive.votv.zip/VDMR/archive-mrdrnose-votv/${version}.7z";
+    url = "https://archive.votv.zip/VDMR/archive-mrdrnose-votv/${_old.version}.7z";
     sha256 = "sha256-3qpCNHhx1PDU5zts1mW3UVfCtjty/7kJawCSx+EV6X8=";
   };
 
-  dontUnpack = true;
   nativeBuildInputs = [ p7zip ];
 
-  inherit wine;
-  wineArch = "win64";
-  enableMonoBootPrompt = false;
-  persistRegistry = false;
-  persistRuntimeLayer = false;
-  fileMapDuringAppInstall = false;
-
-  fileMap = {
-    "$HOME/.local/share/VotV" = "drive_c/users/$USER/AppData/Local/VotV";
-  };
-
-  winAppInstall =
-    # bash
-    ''
-      winetricks -q corefonts
-      winetricks -q fontsmooth=rgb
-      winetricks -q vcrun2022 dxvk
-
-      mkdir -p "${gameDir}"
-      ${lib.getExe ouch} d ${src} -d "${gameDir}"
-    '';
-
-  winAppPreRun =
-    # bash
-    ''
-      mkdir -p "${gameDir}"
-    '';
-
-  winAppRun =
-    # bash
-    ''
-      mkdir -p "${gameDir}"
-      wine start /unix "$WINEPREFIX/drive_c/votv/${version}/WindowsNoEditor/VotV.exe" "$ARGS"
-    '';
-
-  winAppPostRun = toString null;
-
   installPhase = ''
-    runHook preInstall
-    ln -s $out/bin/.launcher $out/bin/${pname}
-    runHook postInstall
+    mkdir -p $out/{bin,share/votv}
+    mv WindowsNoEditor/* $out/share/votv
+
+    ln -s $out/share/votv/VotV.exe $out/bin/votv
+    ln -s $out/share/votv/VotV.exe $out/bin/VotV.exe
+
+    chmod +x $out/share/votv/VotV.exe \
+      $out/bin/votv $out/bin/VotV.exe
   '';
 
   meta = {
-    description = "Voices of the Voice packaged for NixOS";
+    description = "Voices of the Void (windows version)";
     homepage = "https://mrdrnose.itch.io/votv";
-    license = lib.licenses.unfree;
+    license = lib.licenses.agpl3Only;
     platforms = [ "x86_64-linux" ];
     maintainers = with lib.maintainers; [
       {
@@ -79,6 +40,6 @@ mkWindowsApp rec {
         email = "xfalwa@gmail.com";
       }
     ];
-    mainProgram = "hasher";
+    mainProgram = "VotV.exe";
   };
-}
+})
