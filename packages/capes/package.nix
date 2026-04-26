4be9b9babd448a052c7d11cee9702a34abf20d5d
unazikx <xfalwa@gmail.com>
2026-04-26 15:46:04 +0900

    capes: init

---
+++ packages/capes/package.nix
@@ -0,0 +1,39 @@
+{
+  buildGoModule,
+  fetchFromGitHub,
+  pkg-config,
+  imagemagick,
+  lib,
+}:
+
+buildGoModule (_old: {
+  pname = "capes";
+  version = "git";
+
+  src = fetchFromGitHub {
+    owner = "dorochadev";
+    repo = "capes";
+    rev = "55e355e06af2d950402638a1fcaba827089c9a17";
+    hash = "sha256-KMVFppb2aqHsJE90s0mA5+bgMH8JK5ho6gR0Ll0CTf0=";
+  };
+
+  vendorHash = "sha256-4vgmSwtJdtvdP/6qckBORr+eTImcGAaFAKPI4Np9k00=";
+
+  # nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    imagemagick
+  ];
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = {
+    description = "Display minecraft capes and player heads";
+    homepage = "https://github.com/dorochadev/capes";
+    license = lib.licenses.mit;
+    mainProgram = "cliamp";
+  };
+})
