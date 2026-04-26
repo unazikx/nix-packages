a32cb0ae5e8cee8f615c0c46e1f8a104e2d4e3bc
unazikx <xfalwa@gmail.com>
2026-04-26 15:39:07 +0900

    dcfetch: init

---
+++ packages/dcfetch/package.nix
@@ -0,0 +1,43 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  lib,
+}:
+
+stdenv.mkDerivation {
+  pname = baseNameOf ./.;
+  version = "git";
+
+  src = fetchFromGitHub {
+    owner = "tranarchy";
+    repo = "dcfetch";
+    rev = "b4f6ef292649569a9b0d092393953fbbccdb500a";
+    hash = "sha256-jizIqhGTJCcnWmwbjgRAmFihT4SMOo29INe1YgULufg=";
+  };
+
+  buildPhase = ''
+    $CC src/main.c src/dict_util.c src/os.c src/rpc.c \
+        ${
+          if stdenv.isLinux then
+            "src/audio/linux.c"
+          else if stdenv.isDarwin then
+            "src/audio/macos.m"
+          else
+            "src/audio/unsupported.c"
+        } \
+        -Iinclude -Wall -o dcfetch \
+        ${
+          if stdenv.isLinux then
+            "$(pkg-config --cflags --libs libpulse)"
+          else if stdenv.isDarwin then
+            "-framework Foundation -F /System/Library/PrivateFrameworks -weak_framework MediaRemote"
+          else
+            ""
+        }
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -m775 dcfetch $out/bin
+  '';
+}
